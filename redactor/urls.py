a91785b29ec5b6da0c6c1771715d02f247566255
Daria Chernobaieva <daria@douleutaras.com>
2019-10-07 16:59:13 +0300

    Made compatible with Django 2.0

--- redactor/urls.py
+++ redactor/urls.py
@@ -1,12 +1,12 @@
 try:
-    from django.conf.urls.defaults import url, patterns
+    from django.conf.urls.defaults import url
 except ModuleNotFoundError:
-    from django.conf.urls import url, patterns
+    from django.conf.urls import url
 from redactor.views import redactor_upload
 from redactor.forms import FileForm, ImageForm
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url('^upload/image/(?P<upload_to>.*)', redactor_upload, {
         'form_class': ImageForm,
         'response': lambda name, url: '<img src="%s" alt="%s" />' % (url, name),
@@ -16,4 +16,4 @@ urlpatterns = patterns('',
         'form_class': FileForm,
         'response': lambda name, url: '<a href="%s">%s</a>' % (url, name),
     }, name='redactor_upload_file'),
-)
+]
