9e91a0f945e54882d93f985a6b9765a1be9d48bb
Daria Chernobaieva <daria@douleutaras.com>
2020-01-22 14:49:17 +0200

    Compatibility with django 2

--- redactor/widgets.py
+++ redactor/widgets.py
@@ -49,8 +49,8 @@ class RedactorEditor(widgets.Textarea):
         })
         return json.dumps(options)
 
-    def render(self, name, value, attrs=None):
-        html = super(RedactorEditor, self).render(name, value, attrs)
+    def render(self, name, value, attrs=None, renderer=None):
+        html = super(RedactorEditor, self).render(name, value, attrs, renderer)
         final_attrs = self.build_attrs(attrs)
         id_ = final_attrs.get('id')
         html += INIT_JS % (id_, self.get_options())
